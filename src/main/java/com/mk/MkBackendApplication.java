68389c6d3d96aec01abe0924f63ff35ac36d8959
angsong <angsong@wizteco.com>
2019-09-05 21:16:45 +0800

    initinal submit

---
+++ src/main/java/com/mk/MkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mk/MkBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MkBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
